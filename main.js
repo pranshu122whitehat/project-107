fdd93953cbcbd3b97a683b832da65709f5e7d33d
pranshu122whitehat <84527549+pranshu122whitehat@users.noreply.github.com>
2021-05-30 20:50:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+  width:350,
+  height:300,
+  image_format : 'png',
+  png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 verison:', ml5.verison);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/[...]',modelloaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "and the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
